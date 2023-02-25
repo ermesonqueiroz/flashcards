594318dd3057b4f6d27146d5c67dfef3217347a2
Ermeson Sampaio <ermeson.s.queiroz@gmail.com>
2023-02-25 18:13:11 -0300

    Create InvalidTerm Error

---
+++ src/entities/errors/invalid-term.ts
@@ -0,0 +1,8 @@
+import { type DomainError } from './domain-error'
+
+export class InvalidTermError extends Error implements DomainError {
+  public constructor (term: string) {
+    super(`The term "${term}" is invalid.`)
+    this.name = 'InvalidTermError'
+  }
+}
