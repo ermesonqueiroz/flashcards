593f5e793eb5170d633169b8a007c4b6bf5f8f59
Ermeson Sampaio <ermeson.s.queiroz@gmail.com>
2023-02-25 18:13:25 -0300

    Create Term class

---
+++ src/entities/card/term.ts
@@ -0,0 +1,25 @@
+import { type Either, left, right } from '../../common/either'
+import { InvalidTermError } from '../errors/invalid-term'
+
+export class Term {
+  private readonly term: string
+
+  private constructor (term: string) {
+    this.term = term
+  }
+
+  public static create (term: string): Either<InvalidTermError, Term> {
+    if (!this.validate(term)) return left(new InvalidTermError(term))
+
+    return right(new Term(term.trim().replace(/  +/g, ' ')))
+  }
+
+  public static validate (term: string): boolean {
+    if (term.trim().length === 0) return false
+    return true
+  }
+
+  get value (): string {
+    return this.term
+  }
+}
