09b381ff32226a6ff95ea61daca816c4b878c454
Ermeson Sampaio <ermeson.s.queiroz@gmail.com>
2023-02-25 19:18:28 -0300

    feat: implements Definition class in Card entity

--- src/entities/card/card.ts
+++ src/entities/card/card.ts
@@ -1,25 +1,28 @@
 import { type CardData } from './card-data'
 import { left, right, type Either } from '@common/either'
 import { Term } from './term'
-import { InvalidTermError } from '@entities/errors'
+import { Definition } from './definition'
+import { InvalidDefinitionError, InvalidTermError } from '@entities/errors'
 
 export class Card {
   public readonly term: Term
-  public readonly definition: string
+  public readonly definition: Definition
 
-  private constructor (term: Term, definition: string) {
+  private constructor (term: Term, definition: Definition) {
     this.term = term
     this.definition = definition
   }
 
-  public static create ({ term, definition }: CardData): Either<InvalidTermError, Card> {
+  public static create ({ term, definition }: CardData): Either<InvalidTermError | InvalidDefinitionError, Card> {
     const termOrError = Term.create(term)
+    const definitionOrError = Definition.create(definition)
 
     if (termOrError.isLeft()) return left(new InvalidTermError(term))
+    if (definitionOrError.isLeft()) return left(new InvalidDefinitionError(definition))
 
     return right(new Card(
       termOrError.value,
-      definition
+      definitionOrError.value
     ))
   }
 }
