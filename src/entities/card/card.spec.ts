79c3fa7ff0925abde90c68cdbfd0b797e830ea6e
Ermeson Sampaio <ermeson.s.queiroz@gmail.com>
2023-02-25 18:13:50 -0300

    Create Card tests

---
+++ src/entities/card/card.spec.ts
@@ -0,0 +1,42 @@
+import { describe, it, expect } from 'vitest'
+import { right } from '../../common/either'
+import { InvalidTermError } from '../errors/invalid-term'
+import { Card } from './card'
+
+describe('Card domain entity', () => {
+  it('should create card', () => {
+    const card = Card.create({
+      term: 'Foo',
+      definition: 'Bar'
+    })
+
+    expect(card.isRight()).toBeTruthy()
+  })
+
+  it('should not create card with blank definition', () => {
+    const term = '      '
+    const card = Card.create({
+      term,
+      definition: 'Bar'
+    })
+
+    expect(card).toEqual(right(new InvalidTermError(term)))
+  })
+
+  it('should create card without blank spaces in term', () => {
+    const term = '   Foo   !'
+    const card = Card.create({
+      term,
+      definition: 'Bar'
+    })
+
+    const expectedTerm = 'Foo !'
+    const expectedCard = Card.create({
+      term: expectedTerm,
+      definition: 'Bar'
+    })
+
+    expect(card.isRight()).toBeTruthy()
+    expect(card.value).toEqual(expectedCard.value)
+  })
+})
