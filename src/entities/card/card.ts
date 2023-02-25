0f5fa7b2c919395b7cc12d3bc9e9c6822c53dcb2
Ermeson Sampaio <ermeson.s.queiroz@gmail.com>
2023-02-25 18:13:41 -0300

    Create Card class

---
+++ src/entities/card/card.ts
@@ -0,0 +1,25 @@
+import { type CardData } from './card-data'
+import { left, right, type Either } from '../../common/either'
+import { Term } from './term'
+import { InvalidTermError } from '../errors/invalid-term'
+
+export class Card {
+  public readonly term: Term
+  public readonly definition: string
+
+  private constructor (term: Term, definition: string) {
+    this.term = term
+    this.definition = definition
+  }
+
+  public static create ({ term, definition }: CardData): Either<InvalidTermError, Card> {
+    const termOrError = Term.create(term)
+
+    if (termOrError.isLeft()) return left(new InvalidTermError(term))
+
+    return right(new Card(
+      termOrError.value,
+      definition
+    ))
+  }
+}
