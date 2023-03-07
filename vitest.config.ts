a0034b897e1c63e5147da15431c34c5f5cb5e5ab
Ermeson Sampaio <ermeson.s.queiroz@gmail.com>
2023-03-07 19:19:28 -0300

    chore: Add paths to alias

--- vitest.config.ts
+++ vitest.config.ts
@@ -4,7 +4,9 @@ export default defineConfig({
   test: {
     alias: {
       '@common': './src/common',
-      '@entities': './src/entities'
+      '@entities': './src/entities',
+      '@repositories': './src/repositories',
+      '@usecases': './src/usecases'
     }
   }
 })
