fbcdbd0876ebb780aa3d8268aeb78643f54ee377
Ermeson Sampaio <ermeson.s.queiroz@gmail.com>
2023-03-10 19:02:11 -0300

    fix: typo error in invalid title error

--- src/entities/errors/deck/invalid-title.ts
+++ src/entities/errors/deck/invalid-title.ts
@@ -2,7 +2,7 @@ import { type DomainError } from '../domain-error'
 
 export class InvalidTitleError extends Error implements DomainError {
   public constructor (title: string) {
-    super(`The title "${title} is invalid"`)
+    super(`The title "${title}" is invalid.`)
     this.name = 'InvalidTitleError'
   }
 }
