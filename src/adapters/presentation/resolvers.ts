eb25cc1300fe5c5d2c1dc9979ddeb929267156ad
Ermeson Sampaio <ermeson.s.queiroz@gmail.com>
2023-03-19 21:41:00 -0300

    feat: implement resolvers

--- src/adapters/presentation/resolvers.ts
+++ src/adapters/presentation/resolvers.ts
@@ -1,6 +1,8 @@
 import { CreateCardUseCase } from '@usecases/create-card/create-card'
 import { CreateDeckUseCase } from '@usecases/create-deck/create-deck'
+import { CreateDifficultyUseCase } from '@usecases/create-difficulty/create-difficulty'
 import { GetDeckUseCase } from '@usecases/get-deck/get-deck'
+import { MarkCardAsDoneUseCase } from '@usecases/mark-card-as-done/mark-card-as-done'
 import { type Resolvers } from 'src/__generated__/resolvers-types'
 
 export default {
@@ -50,6 +52,34 @@ export default {
       return {
         id: deckOrError.value.id,
         title: deckOrError.value.title
+      }
+    },
+    cardDone: async (_, { cardId: id, difficulty }, contextValue) => {
+      const markCardAsDoneUseCase = new MarkCardAsDoneUseCase(
+        contextValue.dataSources.difficulties,
+        contextValue.dataSources.cards
+      )
+
+      const response = await markCardAsDoneUseCase.execute({
+        id,
+        difficulty
+      })
+
+      if (response.isLeft()) return false
+      return true
+    },
+    addDifficulty: async (_, { name, weight }, contextValue) => {
+      const createDifficultyUseCase = new CreateDifficultyUseCase(contextValue.dataSources.difficulties)
+
+      const difficultyOrError = await createDifficultyUseCase.execute({
+        name,
+        weight
+      })
+
+      if (difficultyOrError.isLeft()) return {}
+      return {
+        name: difficultyOrError.value.name,
+        weight: difficultyOrError.value.weight
       }
     }
   }
