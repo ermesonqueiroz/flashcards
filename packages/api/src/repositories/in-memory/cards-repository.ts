83ab4438b9ede53581153754507afbb2d6a15c97
Ermeson Sampaio <ermeson.s.queiroz@gmail.com>
2023-11-07 23:07:47 -0300

    feat: implement all in memory cards repository methods

--- packages/api/src/repositories/in-memory/cards-repository.ts
+++ packages/api/src/repositories/in-memory/cards-repository.ts
@@ -15,4 +15,24 @@ export class InMemoryCardsRepository implements CardsRepository {
   public async findByDeckId (deckId: string): Promise<CardData[]> {
     return this.items.filter((card) => card.deckId === deckId)
   }
+
+  public async findAll (): Promise<CardData[]> {
+    return this.items
+  }
+
+  public async findById (id: string): Promise<CardData | null> {
+    return this.items.find((card) => card.id === id) ?? null
+  }
+
+  public async update (id: string, data: Partial<Omit<CardData, 'id'>>): Promise<CardData | null> {
+    const index = this.items.findIndex((card) => card.id === id)
+    const card = {
+      ...this.items[index],
+      ...data
+    }
+
+    this.items[index] = card
+
+    return card
+  }
 }
