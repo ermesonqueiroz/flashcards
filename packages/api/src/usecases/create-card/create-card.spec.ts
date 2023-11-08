f068d90f7592cb1086035b4f1d34cd6b73a15c74
Ermeson Sampaio <ermeson.s.queiroz@gmail.com>
2023-11-08 19:36:30 -0300

    fix: create card tests

--- packages/api/src/usecases/create-card/create-card.spec.ts
+++ packages/api/src/usecases/create-card/create-card.spec.ts
@@ -2,13 +2,26 @@ import { left } from '@/common/either'
 import { InvalidDefinitionError, InvalidTermError } from '@/entities/errors/card'
 import { InMemoryCardsRepository } from '@/repositories/in-memory/cards-repository'
 import { CreateCardUseCase } from './create-card'
+import { UuidServiceStub } from '../__mocks__/uuid-service'
+
+afterEach(() => {
+  jest.restoreAllMocks()
+})
+
+function makeSut () {
+  const uuidServiceStub = new UuidServiceStub()
+  const cardsRepository = new InMemoryCardsRepository([])
+  const sut = new CreateCardUseCase(cardsRepository, uuidServiceStub)
+
+  return { uuidServiceStub, sut }
+}
 
 describe('Create card use case', () => {
   it('Should create card', async () => {
-    const cardsRepository = new InMemoryCardsRepository([])
-    const createCardUseCase = new CreateCardUseCase(cardsRepository)
+    const { sut, uuidServiceStub } = makeSut()
+    uuidServiceStub.generate.mockReturnValue('id')
 
-    const response = await createCardUseCase.execute({
+    const response = await sut.execute({
       deckId: 'DECK-ID',
       term: 'Foo',
       definition: 'Bar'
@@ -18,12 +31,11 @@ describe('Create card use case', () => {
   })
 
   it('should not create card with blank term', async () => {
-    const cardsRepository = new InMemoryCardsRepository([])
-
-    const createCardUseCase = new CreateCardUseCase(cardsRepository)
+    const { sut, uuidServiceStub } = makeSut()
+    uuidServiceStub.generate.mockReturnValue('id')
 
     const term = '      '
-    const response = await createCardUseCase.execute({
+    const response = await sut.execute({
       deckId: 'DECK-ID',
       term,
       definition: 'Bar'
@@ -33,19 +45,20 @@ describe('Create card use case', () => {
   })
 
   it('should create card without blank spaces in term', async () => {
-    const cardsRepository = new InMemoryCardsRepository([])
-
-    const createCardUseCase = new CreateCardUseCase(cardsRepository)
+    const { sut, uuidServiceStub } = makeSut()
+    uuidServiceStub.generate.mockReturnValue('id')
 
     const term = '   Foo   !'
-    const response = await createCardUseCase.execute({
+    const response = await sut.execute({
       deckId: 'DECK-ID',
       term,
       definition: 'Bar'
     })
 
+    uuidServiceStub.generate.mockReturnValue('id')
+
     const expectedTerm = 'Foo !'
-    const expectedCard = await createCardUseCase.execute({
+    const expectedCard = await sut.execute({
       deckId: 'DECK-ID',
       term: expectedTerm,
       definition: 'Bar'
@@ -56,12 +69,11 @@ describe('Create card use case', () => {
   })
 
   it('should not create card with blank definition', async () => {
-    const cardsRepository = new InMemoryCardsRepository([])
-
-    const createCardUseCase = new CreateCardUseCase(cardsRepository)
+    const { sut, uuidServiceStub } = makeSut()
+    uuidServiceStub.generate.mockReturnValue('id')
 
     const definition = '      '
-    const card = await createCardUseCase.execute({
+    const card = await sut.execute({
       deckId: 'DECK-ID',
       term: 'Foo',
       definition
