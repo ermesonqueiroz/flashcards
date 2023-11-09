2e6ec14f837fde74ea5aa425463ba5506cfd6f39
Ermeson Sampaio <ermeson.s.queiroz@gmail.com>
2023-11-09 10:31:40 -0300

    refactor: remove cards from GetDeckResponse

--- packages/api/src/usecases/get-deck/get-deck-response.ts
+++ packages/api/src/usecases/get-deck/get-deck-response.ts
@@ -1,6 +1,5 @@
 import { type Either } from '@/common/either'
-import { type CardData } from '@/entities/card'
 import { type DeckData } from '@/entities/deck'
 import { type CannotFindDeckError } from '@/usecases/errors/cannot-find-deck'
 
-export type GetDeckResponse = Either<CannotFindDeckError, DeckData & { cards: CardData[] }>
+export type GetDeckResponse = Either<CannotFindDeckError, DeckData>
