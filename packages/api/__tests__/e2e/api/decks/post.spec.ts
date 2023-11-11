0e6368daa6dda32af88690a9c7d67418c3e0e55e
Ermeson Sampaio <ermeson.s.queiroz@gmail.com>
2023-11-11 17:43:58 -0300

    test: add e2e test suite to create deck endpoint

---
+++ packages/api/__tests__/e2e/api/decks/post.spec.ts
@@ -0,0 +1,21 @@
+import request from 'supertest'
+import app from '@/main/config/app'
+import { PrismaHelper } from '@/external/repositories/prisma'
+
+afterAll(async () => {
+  await PrismaHelper.client.$disconnect()
+})
+
+describe('POST /api/decks', () => {
+  it('should return 200 if a valid title is provided', async () => {
+    const response = await request(app)
+      .post('/api/decks')
+      .send({ title: 'Other Title' })
+
+    expect(response.ok).toBeTruthy()
+    expect(response.body).toHaveProperty('id')
+    expect(response.body).toHaveProperty('title')
+  })
+
+  it.todo('should return 400 if a duplicated title is provided')
+})
